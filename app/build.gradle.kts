2935df3c003785e903767a6171d69e8d718f6e52
MITTNAYT <ismailthewriter@gmail.com>
2026-09-22 18:37:50 +0200

    feat: integrate Firebase google-services.json and enable Google Services plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.google.services)
 }
 
 import java.util.Properties
